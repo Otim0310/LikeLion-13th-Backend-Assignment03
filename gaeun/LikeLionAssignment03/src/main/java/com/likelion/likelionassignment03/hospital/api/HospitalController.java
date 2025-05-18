23c0dc9cc19caa38c75fd885939b4da07f370208
Otim0310 <rkdmsdlaa123@naver.com>
2025-05-19 01:35:03 +0900

    Update HospitalController.java

--- gaeun/LikeLionAssignment03/src/main/java/com/likelion/likelionassignment03/hospital/api/HospitalController.java
+++ gaeun/LikeLionAssignment03/src/main/java/com/likelion/likelionassignment03/hospital/api/HospitalController.java
@@ -11,29 +11,26 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/hospital")
+@RequestMapping("/hospitals")
 public class HospitalController {
 
     private final HospitalService hospitalService;
 
-
-    @PostMapping("/save")
-    public ResponseEntity<String> hospitalSave(@RequestBody HospitalSaveRequestDto hospitalSaveRequestDto) {
-        hospitalService.hospitalSave(hospitalSaveRequestDto);
+    @PostMapping
+    public ResponseEntity<String> saveHospital(@RequestBody HospitalSaveRequestDto requestDto) {
+        hospitalService.saveHospital(requestDto);
         return new ResponseEntity<>("병원 저장!", HttpStatus.CREATED);
     }
 
-
-    @GetMapping("/all")
-    public ResponseEntity<HospitalListResponseDto> hospitalFindAll() {
-        HospitalListResponseDto hospitalListResponseDto = hospitalService.hospitalFindAll();
-        return new ResponseEntity<>(hospitalListResponseDto, HttpStatus.OK);
+    @GetMapping
+    public ResponseEntity<HospitalListResponseDto> getAllHospitals() {
+        HospitalListResponseDto responseDto = hospitalService.getAllHospitals();
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
-
     @GetMapping("/{hospitalId}")
-    public ResponseEntity<HospitalInfoResponseDto> hospitalFindOne(@PathVariable("hospitalId") Long hospitalId) {
-        HospitalInfoResponseDto hospitalInfoResponseDto = hospitalService.hospitalFindOne(hospitalId);
-        return new ResponseEntity<>(hospitalInfoResponseDto, HttpStatus.OK);
+    public ResponseEntity<HospitalInfoResponseDto> getHospitalById(@PathVariable Long hospitalId) {
+        HospitalInfoResponseDto responseDto = hospitalService.getHospitalById(hospitalId);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 }
